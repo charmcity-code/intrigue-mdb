386ac4135ac7869495239c30a8bf51bd25eb5f09
Mark Lawrence <lawrence.ml@gmail.com>
2020-04-07 23:13:35 -0400

    add material design card styling

--- src/containers/EntityDetails.js
+++ src/containers/EntityDetails.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol,
+} from "mdbreact";
 
 class EntityDetails extends Component {
   render() {
@@ -8,7 +17,15 @@ class EntityDetails extends Component {
     if (this.props.index) {
       entity = <h1>{this.props.index}</h1>;
     }
-    return entity;
+    return (
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>{entity}</MDBCardTitle>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    );
   }
 }
 
